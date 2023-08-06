1ec0a0c63b54e8a336f89163ab74811c904948d0
harsh0565 <harshsenger099@gmail.com>
2023-08-06 20:46:31 +0530

    second

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {  createHashRouter , RouterProvider } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <HashRouter>
+    <HashRouter>
         <App />
-        </HashRouter> */}
-        <RouterProvider router={router} />
+        </HashRouter>
+        
   </React.StrictMode>
 );
 reportWebVitals();
